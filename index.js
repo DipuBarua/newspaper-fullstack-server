8a92ad39e5f6633bd2e9808e3f2df3f3bc2f7c5d
DipuBarua <you@example.com>
2023-11-25 22:52:12 +0600

    updated post api for article uploading

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
             res.send(result);
         })
 
+        app.post("/articles", async (req, res) => {
+            const article = req.body;
+            const result = await articleCollection.insertOne(article);
+            res.send(result);
+        })
+
 
 
         // Send a ping to confirm a successful connection
